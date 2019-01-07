e31fc1541f6105d44cf072f27e0b3b69b626c54e
Vuong-Kevin-216388019 <vuongkev@my.yorku.ca>
2019-01-06 23:27:44 -0500

    Lab 0 part 1
    The tasks that needed to be completed in Lab 0, task 6, 2.2 still needs to be finished

---
+++ Lab0Part1/src/ComputePerimeterAndAreaOfCircle.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class ComputePerimeterAndAreaOfCircle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Enter an integer for radius");
+		double radius = input.nextDouble();
+		input.nextLine();
+		
+		System.out.println("What's your name");
+		String name = input.nextLine();
+		
+		
+		double perimeter = 2 * 3.14 * radius;
+		double area = 3.14 * Math.pow(radius, 2); 
+		
+		
+		System.out.println(name + ", your input circle with radius " + radius + " has a perimeter " + perimeter + " and area " + area);
+		
+		input.close();
+		
+	}
+
+}
---
+++ Lab0Part1/src/ConvertSecondsIntoMinutes.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class ConvertSecondsIntoMinutes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("What's your name?");
+		String name = input.nextLine();
+		
+		
+		System.out.println("Enter an integer for seconds");
+		double seconds = input.nextDouble();
+		input.nextLine();
+		
+		
+		double minutes = 
+		double secondss = 
+		
+		
+		System.out.println(name + ", your input is " + seconds + " seconds can be divided into " + minutes + " minutes and " + secondss + " seconds");
+		
+		input.close();
+	}
+
+}
